4fc359cfbb48df4cdc4cfa935380e5e69987e85f
alchemistmatt <matthew.monroe@pnnl.gov>
2021-10-29 17:40:38 -0700

    Update batch file to look for executable in multiple locations

--- ExampleParameterFiles/Run_Program.bat
+++ ExampleParameterFiles/Run_Program.bat
@@ -1,1 +1,23 @@
-..\bin\MaxQuantParamFileModExtractor.exe *.xml > ParamFileMods.txt
+@echo off
+
+set ExePath=MaxQuantParamFileModExtractor.exe
+
+if exist %ExePath% goto DoWork
+if exist ..\%ExePath% set ExePath=..\%ExePath% && goto DoWork
+if exist ..\bin\%ExePath% set ExePath=..\bin\%ExePath% && goto DoWork
+
+echo Executable not found: %ExePath%
+goto Done
+
+:DoWork
+echo.
+echo Processing with %ExePath%
+echo.
+
+%ExePath% *.xml
+
+%ExePath% *.xml > ParamFileMods.txt
+
+:Done
+
+pause

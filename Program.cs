e12ab98e18d24100c2fa61bc987c11e06af518ef
alchemistmatt <matthew.monroe@pnnl.gov>
2021-11-11 17:48:00 -0800

    Rename variables and update program description

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace MaxQuantParamFileModExtractor
     /// </summary>
     internal static class Program
     {
-        public const string PROGRAM_DATE = "October 29, 2021";
+        public const string PROGRAM_DATE = "November 11, 2021";
 
         // Ignore Spelling: Conf
 
@@ -19,31 +19,38 @@ namespace MaxQuantParamFileModExtractor
             var programName = System.Reflection.Assembly.GetEntryAssembly()?.GetName().Name;
             var exePath = PRISM.FileProcessor.ProcessFilesOrDirectoriesBase.GetAppPath();
             var exeName = Path.GetFileName(exePath);
-            var cmdLineParser = new CommandLineParser<ModExtractorOptions>(programName, GetAppVersion())
+
+            var parser = new CommandLineParser<ModExtractorOptions>(programName, GetAppVersion())
             {
                 ProgramInfo = ConsoleMsgUtils.WrapParagraph(
-                                   "This program parses a MaxQuant parameter file (XML-based) to extract the nodes that define static and dynamic mods."),
-                ContactInfo = "Program written by Matthew Monroe for PNNL (Richland, WA) in 2021" + Environment.NewLine +
-                                             "E-mail: matthew.monroe@pnnl.gov or proteomics@pnnl.gov" + Environment.NewLine +
-                                             "Website: https://github.com/PNNL-Comp-Mass-Spec/ or https://panomics.pnnl.gov/ or https://www.pnnl.gov/integrative-omics"
+                              "This program parses a MaxQuant parameter file (XML-based) to extract the nodes that define static and dynamic mods."),
+                ContactInfo = "Program written by Matthew Monroe for PNNL (Richland, WA)" + Environment.NewLine +
+                              "E-mail: matthew.monroe@pnnl.gov or proteomics@pnnl.gov" + Environment.NewLine +
+                              "Website: https://github.com/PNNL-Comp-Mass-Spec/ or https://panomics.pnnl.gov/ or https://www.pnnl.gov/integrative-omics"
             };
 
             // ReSharper disable StringLiteralTypo
 
-            cmdLineParser.UsageExamples.Add(exeName + " MaxQuant_Tryp_Stat_CysAlk_Dyn_MetOx_NTermAcet_20ppmParTol.xml");
-            cmdLineParser.UsageExamples.Add(exeName + " /I:MaxQuant*.xml");
+            parser.UsageExamples.Add(exeName + " MaxQuant_Tryp_Stat_CysAlk_Dyn_MetOx_NTermAcet_20ppmParTol.xml");
+            parser.UsageExamples.Add(exeName + " /I:MaxQuant*.xml");
 
             // ReSharper restore StringLiteralTypo
 
             // The default argument name for parameter files is /ParamFile or -ParamFile
             // Also allow /Conf or /P
-            cmdLineParser.AddParamFileKey("Conf");
-            cmdLineParser.AddParamFileKey("P");
+            parser.AddParamFileKey("Conf");
+            parser.AddParamFileKey("P");
 
-            var result = cmdLineParser.ParseArgs(args);
+            var result = parser.ParseArgs(args);
             var options = result.ParsedResults;
+
             if (!result.Success || !options.Validate())
             {
+                if (parser.CreateParamFileProvided)
+                {
+                    return 0;
+                }
+
                 // Delay for 750 msec in case the user double clicked this file from within Windows Explorer (or started the program via a shortcut)
                 System.Threading.Thread.Sleep(750);
                 return -1;

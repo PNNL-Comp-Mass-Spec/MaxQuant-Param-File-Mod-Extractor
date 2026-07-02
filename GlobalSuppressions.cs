bc5fd84dd5fbed73fc0b8b2f974b11685790172c
alchemistmatt <matthew.monroe@pnnl.gov>
2026-07-02 16:17:31 -0700

    Suppress warnings

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,10 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Roslynator", "RCS1235:Optimize method call", Justification = "Leave as-is", Scope = "member", Target = "~M:MaxQuantParamFileModExtractor.MaxQuantModExtractor.ProcessFile(System.String)~System.Boolean")]
+[assembly: SuppressMessage("Style", "IDE0290:Use primary constructor", Justification = "Prefer current style", Scope = "member", Target = "~M:MaxQuantParamFileModExtractor.MaxQuantModExtractor.#ctor(MaxQuantParamFileModExtractor.ModExtractorOptions)")]
+[assembly: SuppressMessage("Style", "IDE0300:Simplify collection initialization", Justification = "Leave as-is", Scope = "member", Target = "~M:MaxQuantParamFileModExtractor.MaxQuantModExtractor.ProcessFile(System.String)~System.Boolean")]
